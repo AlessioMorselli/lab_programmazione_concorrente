9f4b36bdecb9f085e221bb09b61d1a6ce12e68e3
Alessio <ale.mors.22@gmail.com>
2018-08-03 10:35:38 +0200

    Routing configurato

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ### URL DI DEFAULT ###
+  resources :gruops do
+    resources :events, except: [:show]
+    resources :members, only: [:index]
+    resources :messages, except: [:show, :new, :edit]
+    resources :invitations, only: [:new, :create, :delete]
+  end
+  resources :courses, only: [:show, :index]
+  resources :degree, only: [:show]
+  resources :users, except: [:new, :create, :show] do
+    resources :events, only: [:index, :show]
+    resources :invitations, only: [:index]
+  end
+
+  ### URL NON DI DEFAULT ###
+  get     '/signup',                                              to: 'users#new'
+  post    '/signup',                                              to: 'users#create'
+
+  get     '/',                                                    to: 'sessions#new'
+  get     '/login',                                               to: 'sessions#new'
+  post    '/login',                                               to: 'sessions#create'
+  delete  '/logout',                                              to: 'sessions#destroy'
+
+  get     '/gruops/:gruop_id/messages/pinned',                    to: 'messages#pinned', as: 'group_pinned_messages'
 end
