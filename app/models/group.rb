cb83ff96debd73127536397fea608171ed238bcd
AlexCovizzi <alexcovizzi@gmail.com>
2018-08-06 17:59:12 +0200

    aggiunti nella classe gruppi validazione e relazioni

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,12 @@
 class Group < ApplicationRecord
+    validates :name, presence: true
+    validates :max_members, numericality: { only_integer: true, greater_than_or_equal_to: -1, other_than: 0 }
+
+    has_one :course
+
+    has_many :memberships, :dependent => :delete_all
+    has_many :members, class_name: "User", through: :memberships, :source => :user
+
+    has_many :invitations, :dependent => :delete_all
+    has_many :invitees, class_name: "User", through: :invitations, :source => :user
 end
