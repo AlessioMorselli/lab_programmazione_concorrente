6a96fbb567dc0d0912ed75160cdcb2b0142f7334
AlexCovizzi <alexcovizzi@gmail.com>
2018-08-11 11:59:09 +0200

    messo il valore di default in invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,8 @@ class Invitation < ApplicationRecord
     belongs_to :group
 
     validates_uniqueness_of :user_id, :scope => :group_id
+
+    before_save do
+        self.url_string = url_string.presence || SecureRandom.urlsafe_base64
+    end
 end
