118b01d136a5d37db6b8610d69f80b79d4f55e92
AlexCovizzi <alexcovizzi@gmail.com>
2018-09-24 09:47:41 +0200

    aggiunto utente admin placeholder ai gruppi ufficiali

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,21 @@ file = File.read(file_path)
 degree_list = JSON.parse(file)
 puts "Loaded: "+file_path
 
+u = User.create!(email: "adminbot@unife.studygroups.it", name: "adminbot", password: "adminbot")
+
 degree_list.each do |degree|
+  if degree["name"].include? "Ingegneria"
     d = Degree.create!(name: degree["name"], years: degree["num_years"])
     
     degree["years"].each do |year|
         year["courses"].each do |course|
             c = Course.create!(name: course["name"])
-            g = Group.create!(name: "#{course["name"]}", course_id: c.id, private: true, description: "Gruppo ufficiale di #{course["name"]} del corso di studio #{degree["name"]} (Anno #{year["year"]})")
+            g = Group.new(name: "#{course["name"]}", course_id: c.id, private: true, description: "Gruppo ufficiale di #{course["name"]} del corso di studio #{degree["name"]} (Anno #{year["year"]})")
+            g.save_with_admin(u)
             DegreeCourse.create!(degree_id: d.id, course_id: c.id, year: year["year"], group_id: g.id)
         end
     end
 
     puts "Inserted degree: #{degree["name"]}"
+  end
 end
